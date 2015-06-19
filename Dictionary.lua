af6de6e38e96fe3d74dc492d70f9a9a823c955c4
nicholas-leonard <nick@nikopia.org>
2015-06-19 09:47:13 -0400

    Dictionary works with LookupTable version 4

--- Dictionary.lua
+++ Dictionary.lua
@@ -3,11 +3,32 @@ local Dictionary, parent = torch.class("nn.Dictionary", "nn.LookupTable")
 -- don't use this with optim (useless), use nn.LookupTable instead
 function Dictionary:__init(dictSize, embeddingSize, accUpdate)
    parent.__init(self, dictSize, embeddingSize)
+   assert(parent.__version >= 4, "Update the nn package to newest version")
    if accUpdate then -- this could save you lots of memory
-      self.gradWeight = nil
+      self:accUpdateOnly()
+   else
+      self.gradWeight:zero()
    end
    self.accUpdate = accUpdate
    self.dpnn_sparseParameters = true --disable for use with optim 
+   self.inputs = {}
+end
+
+function Dictionary:accUpdateOnly()
+   self.accUpdate = false
+   return parent.accUpdateOnly(self)
+end
+
+function Dictionary:updateOutput(input)
+   assert(torch.type(input) ~= 'torch.CudaTensor', "Expecting non-CudaTensor input")
+   return parent.updateOutput(self, input)
+end
+
+function Dictionary:accGradParameters(input, gradOutput, scale)
+   self.nBackward = (self.nBackward or 0) + input:nElement()
+   -- this is why we dont want cuda inputs
+   input:apply(function(k) self.inputs[k] = (self.inputs[k] or 0) + 1 end)
+   return parent.accGradParameters(self, input, gradOutput, scale)
 end
 
 function Dictionary:parameters()
@@ -15,12 +36,11 @@ function Dictionary:parameters()
       -- only return the parameters affected by the forward/backward
       local params, gradParams, scales = {}, {}, {}
       for k,nBackward in pairs(self.inputs) do
-         local kscale = self:scaleUpdateByKey(k)
          params[k] = self.weight:select(1, k)
          if self.gradWeight then
             gradParams[k] = self.gradWeight:select(1, k)
          end
-         scales[k] = self:scaleUpdateByKey(k)
+         scales[k] = 1
       end
       return params, gradParams, scales, self.weight:size(1)
    end
@@ -65,6 +85,16 @@ function Dictionary:getParameters()
    
    self.dpnn_sparseParameters = sp
    return flatParameters, flatGradParameters
+end
+
+function Dictionary:zeroGradParameters()
+   if not self.accUpdate then
+      for k,_ in pairs(self.inputs) do
+         self.gradWeight:select(1, k):zero()
+      end
+   end
+   self.inputs = {}
+   self.nBackward = 0
 end
 
    
