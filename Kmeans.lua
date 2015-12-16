c66cd93c02ac7445ca7845e2a661e87f95f37dcf
Sagar_M_Waghmare <SW@discoverelement.com>
2015-12-16 15:25:01 -0500

    Kmeans: completed forward and backward. Centroids updated in accGradParameters and then gradWeight set to zero so that it does not change centroids during gradient descent if Kmeans used in a model with optim for training..

--- Kmeans.lua
+++ Kmeans.lua
@@ -15,7 +15,9 @@ function Kmeans:__init(k, dim, mean, std, centers)
       self.centers = torch.Tensor()
       self.centers:resize(self.k, self.dim)
       self.centers:normal(self.mean, self.std)
-   end 
+   end
+   self.gradWeight = self.centers.new()
+   self.gradWeight:resizeAs(centers)
 end
 
 function Kmeans:updateOutput(input)
@@ -29,6 +31,10 @@ function Kmeans:updateOutput(input)
    self.output = self.output or self.centers.new()
    self.output:resize(noOfSamples)
 
+   self.gradWeight:zero()
+   self._clusterSampleCount = self._clusterSampleCount or self.centers.new()
+   self._clusterSampleCount:resize(self.k):zero()
+
    -- a sample copied k times to compute distance between sample and centers
    self._expandedSample = self._expandedSample or self.centers.new()
 
@@ -38,19 +44,65 @@ function Kmeans:updateOutput(input)
    self._temp = self._temp or input.new()
    self._minScore = self._minScore or self.centers.new()
    self._minIndx = self._minIndx or self.centers.new()
+   local clusterIndx
    for i=1, noOfSamples do
       self._temp:expand(input[{{i}}], self.k, self.dim)
       self._expandedSample:resize(self._temp:size()):copy(self._temp)
 
-      -- Squared Euclidean distance
+      -- (x-c)
       self._expandedSample:add(-1, self.centers)
+      -- Euclidean distance ||x-c||
       self._clusterDistances:norm(self._expandedSample, 2, 2)
+      -- Squared Euclidean distance ||x-c||^2
       self._clusterDistances:pow(2)
 
       self._minScore, self._minIndx = self._clusterDistances:min(1)
-      self.output[i] = self._minIndx[1][1]
+      clusterIndx = self._minIndx[1][1]
+      self.output[i] = clusterIndx
+
+      -- center update rule c -> c + 1/n (x-c)
+      -- Saving (x-c). Negate gradWeight in accGradParameters since
+      -- we use negative gradient in gradient descent.
+      self.gradWeight[clusterIndx]:add(self._expandedSample[clusterIndx])
+      self._clusterSampleCount[clusterIndx] = 
+                                    self._clusterSampleCount[clusterIndx] + 1
    end
    return self.output
+end
+
+-- Kmeans has its own criterion hence no gradInput is generated
+function Kmeans:updateGradInput(input, gradOuput)
+   return
+end
+
+-- We define kmeans update rule as c -> c + beta * 1/n * sum_i (x-c).
+-- n is no. of x's belonging to c.
+-- With this update rule you can do online as well full update.
+-- We are saving (x-c) in gradWeight in updateOutput.
+-- We will update centers in this function and then set gradWeight to zero so
+-- that gradient descent update won't make any change.
+function Kmeans:accGradParameters(input, gradOutput, beta)
+   local beta = beta or 1
+   self.beta = beta
+   assert(self.beta <= 1, "Beta greater than 1.")
+
+   self.alpha = 1 - self.beta
+
+   -- 1/n * sum_i (x-c)
+   for i=1, self.k do
+      if self._clusterSampleCount[i] > 0 then
+         self.gradWeight[i]:div(self._clusterSampleCount[i])
+      end
+   end
+
+   -- beta * 1/n * sum_i (x-c)
+   self.gradWeight:mul(beta)
+
+   -- Update kmeans centers
+   self.centers:add(self.gradWeight)
+
+   -- zeroing gradWeight so the gradient descent update won't affect centers.
+   self.gradWeight:zero()
 end
 
 function Kmeans:type(type, tensorCache)
@@ -61,6 +113,7 @@ function Kmeans:type(type, tensorCache)
       self._minIndx = nil
       self._expandedSample = nil
       self._clusterDistances = nil
+      self._clusterSampleCount = nil
    end
    return parent.type(self, type, tensorCache)
 end
