8c55e2251f745a0327bdd133e8be011d5d6752a1
Sagar M Waghmare <SW@discoverelement.com>
2016-01-20 17:36:07 -0500

    Added option to combine training and validation data. Added attempts option. Runs 'attempts' independent experiments.

--- tutorials/ladder.lua
+++ tutorials/ladder.lua
@@ -40,6 +40,8 @@ op:option{'--weightTied', action='store_true', dest='weightTied',
           help='Tie weights of decoder with encoder.', default=false}
 
 -- Criterion and learning
+op:option{'--attempts', action='store', dest='attempts',
+          help='Run attempts independent experiments.', default=1}
 op:option{'--eta', action='store', dest='eta',
           help='If zero then only classifier cost is considered.', default=0}
 op:option{'--batchSize', action='store', dest='batchSize',
@@ -82,7 +84,7 @@ op:summarize()
 
 -- Data
 datadir = opt.datadir
-
+noValidation = opt.noValidation
 trDataFile = paths.concat(datadir, "trainDict.t7")
 tvDataFile = paths.concat(datadir, "validDict.t7")
 tsDataFile = paths.concat(datadir, "testDict.t7")
@@ -119,268 +121,283 @@ for i=1,#inputHiddens do
 end
 hiddens[#hiddens+1] = noOfClasses
 
--- encoder input
-if noiseSigma ~= 0 then
-   if verbose then print("Add noise to the samples.") end
-   input = nn.WhiteNoise(0, noiseSigma)()
-else
-   input = nn.Identity()()
-end
+attempts = tonumber(opt.attempts)
+testAccus = torch.zeros(attempts)
+for attempt=1,attempts do
 
--- encoder model
-encoderLayers = {}
-Zs = {}
-Zs[0] = input
-for i=1,#hiddens-1 do
-   encoderLayers[i] = nn.Linear(hiddens[i], hiddens[i+1])
-   if i==1 then
-      if useBatchNorm then
-         Zs[i] = nn.BatchNormalization(hiddens[i+1])(encoderLayers[i](input))
-      else
-         Zs[i] = encoderLayers[i](input)
-      end
+   -- encoder input
+   if noiseSigma ~= 0 then
+      if verbose then print("Add noise to the samples.") end
+      input = nn.WhiteNoise(0, noiseSigma)()
    else
-      if useBatchNorm then
-         Zs[i] = nn.BatchNormalization(hiddens[i+1])
-                                      (encoderLayers[i](nn.ReLU()(Zs[i-1])))
+      input = nn.Identity()()
+   end
+
+   -- encoder model
+   encoderLayers = {}
+   Zs = {}
+   Zs[0] = input
+   for i=1,#hiddens-1 do
+      encoderLayers[i] = nn.Linear(hiddens[i], hiddens[i+1])
+      if i==1 then
+         if useBatchNorm then
+            Zs[i] = nn.BatchNormalization(hiddens[i+1])(encoderLayers[i](input))
+         else
+            Zs[i] = encoderLayers[i](input)
+         end
       else
-         Zs[i] = encoderLayers[i](nn.ReLU()(Zs[i-1]))
+         if useBatchNorm then
+            Zs[i] = nn.BatchNormalization(hiddens[i+1])
+                                         (encoderLayers[i](nn.ReLU()(Zs[i-1])))
+         else
+            Zs[i] = encoderLayers[i](nn.ReLU()(Zs[i-1]))
+         end
       end
    end
-end
 
--- classifier
-classifier = nn.LogSoftMax()(Zs[#Zs])
+   -- classifier
+   classifier = nn.LogSoftMax()(Zs[#Zs])
 
--- Decoder
-decoderLayers = {}
-Z_hats = {}
-Z_hats[#hiddens+1] = Zs[#Zs]
-for i=#hiddens,2,-1 do
-   decoderLayers[i] = nn.Linear(hiddens[i], hiddens[i-1])
-   if weightTied then
-      if verbose then print("Tying encoder-decoder weights.") end
-      decoderLayers[i].weight:set(encoderLayers[i-1].weight:t())
-      decoderLayers[i].gradWeight:set(encoderLayers[i-1].gradWeight:t())
+   -- Decoder
+   decoderLayers = {}
+   Z_hats = {}
+   Z_hats[#hiddens+1] = Zs[#Zs]
+   for i=#hiddens,2,-1 do
+      decoderLayers[i] = nn.Linear(hiddens[i], hiddens[i-1])
+      if weightTied then
+         if verbose then print("Tying encoder-decoder weights.") end
+         decoderLayers[i].weight:set(encoderLayers[i-1].weight:t())
+         decoderLayers[i].gradWeight:set(encoderLayers[i-1].gradWeight:t())
+      end
+      if useBatchNorm then
+         u = nn.ReLU()(nn.BatchNormalization(hiddens[i-1])
+                                            (decoderLayers[i](Z_hats[i+1])))
+      else
+         u = nn.ReLU()(decoderLayers[i](Z_hats[i+1]))
+      end
+
+      cu1 = nn.CMul(hiddens[i-1])(u)
+      du1 = nn.Add(hiddens[i-1])(u)
+      a1 = nn.CAddTable()({cu1, du1})
+      cu2 = nn.CMul(hiddens[i-1])(u)
+      du2 = nn.Add(hiddens[i-1])(u)
+      a2 = nn.CAddTable()({cu2, du2})
+      cu3 = nn.CMul(hiddens[i-1])(u)
+      du3 = nn.Add(hiddens[i-1])(u)
+      a3 = nn.CAddTable()({cu3, du3})
+      cu4 = nn.CMul(hiddens[i-1])(u)
+      du4 = nn.Add(hiddens[i-1])(u)
+      a4 = nn.CAddTable()({cu4, du4})
+      cu5 = nn.CMul(hiddens[i-1])(u)
+      du5 = nn.Add(hiddens[i-1])(u)
+      a5 = nn.CAddTable()({cu5, du5})
+
+      z_hat1 = nn.CMulTable()({a1, Zs[i-2]})
+      z_hat2 = nn.CMulTable()({a3, Zs[i-2]})
+      z_hat3 = nn.Sigmoid()(nn.CAddTable()({z_hat2, a4}))
+      z_hat4 = nn.CMulTable()({a2, z_hat3})
+      Z_hats[i] = nn.CAddTable()({z_hat1, z_hat4, a5})
    end
-   if useBatchNorm then
-      u = nn.ReLU()(nn.BatchNormalization(hiddens[i-1])
-                                         (decoderLayers[i](Z_hats[i+1])))
-   else
-      u = nn.ReLU()(decoderLayers[i](Z_hats[i+1]))
-   end
+   model = nn.gModule({input}, {classifier, Z_hats[2]--[[Decoder--]]})
+   print(model)
 
-   cu1 = nn.CMul(hiddens[i-1])(u)
-   du1 = nn.Add(hiddens[i-1])(u)
-   a1 = nn.CAddTable()({cu1, du1})
-   cu2 = nn.CMul(hiddens[i-1])(u)
-   du2 = nn.Add(hiddens[i-1])(u)
-   a2 = nn.CAddTable()({cu2, du2})
-   cu3 = nn.CMul(hiddens[i-1])(u)
-   du3 = nn.Add(hiddens[i-1])(u)
-   a3 = nn.CAddTable()({cu3, du3})
-   cu4 = nn.CMul(hiddens[i-1])(u)
-   du4 = nn.Add(hiddens[i-1])(u)
-   a4 = nn.CAddTable()({cu4, du4})
-   cu5 = nn.CMul(hiddens[i-1])(u)
-   du5 = nn.Add(hiddens[i-1])(u)
-   a5 = nn.CAddTable()({cu5, du5})
+   -- Criterion and learning
+   -- Criterion
+   eta = tonumber(opt.eta)
+   criterions = nn.ParallelCriterion()
+   nll = nn.ClassNLLCriterion()
+   mse = nn.MSECriterion()
+   criterions:add(nll)
+   criterions:add(mse, eta)
 
-   z_hat1 = nn.CMulTable()({a1, Zs[i-2]})
-   z_hat2 = nn.CMulTable()({a3, Zs[i-2]})
-   z_hat3 = nn.Sigmoid()(nn.CAddTable()({z_hat2, a4}))
-   z_hat4 = nn.CMulTable()({a2, z_hat3})
-   Z_hats[i] = nn.CAddTable()({z_hat1, z_hat4, a5})
-end
-model = nn.gModule({input}, {classifier, Z_hats[2]--[[Decoder--]]})
-print(model)
+   -- Learning
+   batchSize = tonumber(opt.batchSize)
+   epochs = tonumber(opt.epochs)
+   maxTries = tonumber(opt.maxTries)
+   learningRate = tonumber(opt.learningRate)
+   learningRateDecay = tonumber(opt.learningRateDecay)
+   linearDecay = opt.linearDecay
+   startEpoch = tonumber(opt.startEpoch)
+   endLearningRate = tonumber(opt.endLearningRate)
 
--- Criterion and learning
--- Criterion
-eta = tonumber(opt.eta)
-criterions = nn.ParallelCriterion()
-nll = nn.ClassNLLCriterion()
-mse = nn.MSECriterion()
-criterions:add(nll)
-criterions:add(mse, eta)
-
--- Learning
-batchSize = tonumber(opt.batchSize)
-epochs = tonumber(opt.epochs)
-maxTries = tonumber(opt.maxTries)
-learningRate = tonumber(opt.learningRate)
-learningRateDecay = tonumber(opt.learningRateDecay)
-linearDecay = opt.linearDecay
-startEpoch = tonumber(opt.startEpoch)
-endLearningRate = tonumber(opt.endLearningRate)
-
-if linearDecay then
-   if verbose then print("Using linear decay.") end
-   learningRates = torch.zeros(startEpoch):fill(learningRate)
-   local temp = torch.range(learningRate, endLearningRate,
-                            -learningRate/(epochs-startEpoch))
-   learningRates = torch.cat(learningRates, temp)
-end
-
-momentum = tonumber(opt.momentum)
-loss = opt.loss
-adam = opt.adam
-
--- Optimizer
-optimState = {
-               coefL1 = 0,
-               coefL2 = 0,
-               learningRate = learningRate,
-               weightDecay = 0.0,
-               momentum = momentum,
-               learningRateDecay = learningRateDecay
-             }
-
--- If true use Adaptive moment estimation else SGD.
-if adam then
-   if verbose then print("Using Adaptive moment estimation optimizer.") end
-   optimMethod = optim.adam
-else
-   if verbose then print("Using Stocastic gradient descent optimizer.") end
-   optimMethod = optim.sgd
-end
-if verbose then
-   print(optimMethod)
-   print(optimState)
-end
-
--- Cuda
-useCuda = opt.useCuda
-deviceId = tonumber(opt.deviceId)
-
-if useCuda then
-   if verbose then print("Using GPU: "..deviceId) end
-   cutorch.setDevice(deviceId)
-   if verbose then print("GPU set") end
-   model:cuda()
-   if verbose then print("Model copied to GPU.") end
-   criterions:cuda()
-   if verbose then print("Criterion copied to GPU.") end
-else
-   if verbose then print("Not using GPU.") end
-end
-
--- Retrieve parameters and gradients
-parameters, gradParameters = model:getParameters()
-
--- Reshape samples from images to vectors
-trData.data = trData.data:reshape(trData.size(1), linFeats)
-tvData.data = tvData.data:reshape(tvData.size(1), linFeats)
-tsData.data = tsData.data:reshape(tsData.size(1), linFeats)
-collectgarbage()
-
-if verbose then
-   print(trData)
-   print(tvData)
-   print(tsData)
-end
-
--- Training
-displayProgress = verbose
-classifierIndx = 1
-trainAccu = 0
-validAccu = 0
-bestTrainAccu = 0
-bestValidAccu = 0
-trainLoss = 0
-validLoss = 0
-bestTrainLoss = math.huge
-bestValidLoss = math.huge
-bestTrainModel = nn.Sequential()
-bestValidModel = nn.Sequential()
-earlyStopCount = 0
-for i=1, epochs do
    if linearDecay then
-      optimState.learningRate = learningRates[i]
-   end
-   -- Training
-   trainLoss = model_train_multi_criterion(model, criterions,
-                                           parameters, gradParameters, trData,
-                                           optimMethod, optimState, batchSize,
-                                           i, confusion, trainLogger,
-                                           useCuda, displayProgress,
-                                           classiferIndx)
-   confusion:updateValids()
-   if loss then
-      if verbose then
-         print("Current train loss: ".. trainLoss
-                  ..", best train loss: " .. bestTrainLoss)
-      end
-      if trainLoss < bestTrainLoss then
-         bestTrainLoss = trainLoss
-         bestTrainModel = model:clone()
-         print(confusion)
-      end
-   else -- Using classification accuracy for saving best train model
-      trainAccu = confusion.totalValid * 100
-      if bestTrainAccu < trainAccu then
-         bestTrainAccu = trainAccu
-         bestTrainModel = model:clone()
-         bestTrainLoss = trainLoss
-      end
-      if verbose then
-         print("Current train accu: ".. trainAccu
-                  ..", best train accu: " .. bestTrainAccu
-                  ..", best train loss: " .. bestTrainLoss)
-      end
+      if verbose then print("Using linear decay.") end
+      learningRates = torch.zeros(startEpoch):fill(learningRate)
+      local temp = torch.range(learningRate, endLearningRate,
+                               -learningRate/(epochs-startEpoch))
+      learningRates = torch.cat(learningRates, temp)
    end
 
-   -- Validating
-   validLoss = model_test_multi_criterion(model, criterions,
-                                          tvData, confusion,
-                                          useCuda, classifierIndx)
-   confusion:updateValids()
-   if loss then
-      if verbose then
-         print("Current valid loss: ".. validLoss
-                  ..", best valid loss: " .. bestValidLoss)
-      end
-      if validLoss < bestValidLoss then
-         earlyStopCount = 0
-         bestValidLoss = validLoss
-         bestValidModel = model:clone()
-         print(confusion)
-      else
-         earlyStopCount = earlyStopCount + 1
-      end
+   momentum = tonumber(opt.momentum)
+   loss = opt.loss
+   adam = opt.adam
+
+   -- Optimizer
+   optimState = {
+                  coefL1 = 0,
+                  coefL2 = 0,
+                  learningRate = learningRate,
+                  weightDecay = 0.0,
+                  momentum = momentum,
+                  learningRateDecay = learningRateDecay
+                }
+
+   -- If true use Adaptive moment estimation else SGD.
+   if adam then
+      if verbose then print("Using Adaptive moment estimation optimizer.") end
+      optimMethod = optim.adam
    else
-      validAccu = confusion.totalValid * 100
-      if bestValidAccu < validAccu then
-         earlyStopCount = 0
-         bestValidAccu = validAccu
-         bestValidModel = model:clone()
-         bestValidLoss = validLoss
-      else
-         earlyStopCount = earlyStopCount + 1
-      end
-      if verbose then
-         print("Current valid accu: ".. validAccu
-               ..", best valid accu: " .. bestValidAccu
-               ..", best valid loss: " .. bestValidLoss)
-      end
+      if verbose then print("Using Stocastic gradient descent optimizer.") end
+      optimMethod = optim.sgd
    end
-   print(noiseSigma, weightTied, useBatchNorm, eta, earlyStopCount)
+   if verbose then
+      print(optimMethod)
+      print(optimState)
+   end
 
-   if maxTries ~= 0 then
-      if earlyStopCount >= maxTries then
-         if verbose then print("Early stopping at epoch: " .. i) end
-         break
+   -- Cuda
+   useCuda = opt.useCuda
+   deviceId = tonumber(opt.deviceId)
+
+   if useCuda then
+      if verbose then print("Using GPU: "..deviceId) end
+      cutorch.setDevice(deviceId)
+      if verbose then print("GPU set") end
+      model:cuda()
+      if verbose then print("Model copied to GPU.") end
+      criterions:cuda()
+      if verbose then print("Criterion copied to GPU.") end
+   else
+      if verbose then print("Not using GPU.") end
+   end
+
+   -- Retrieve parameters and gradients
+   parameters, gradParameters = model:getParameters()
+
+   -- Reshape samples from images to vectors
+   trData.data = trData.data:reshape(trData.size(1), linFeats)
+   tvData.data = tvData.data:reshape(tvData.size(1), linFeats)
+   tsData.data = tsData.data:reshape(tsData.size(1), linFeats)
+   collectgarbage()
+
+   if noValidation then
+      trData.data = torch.cat(trData.data, tvData.data, 1)
+      trData.labels = torch.cat(trData.labels, tvData.labels, 1)
+      tvData = nil
+      collectgarbage()
+   end
+
+   if verbose then
+      print(trData)
+      print(tvData)
+      print(tsData)
+   end
+
+   -- Training
+   displayProgress = verbose
+   classifierIndx = 1
+   trainAccu = 0
+   validAccu = 0
+   bestTrainAccu = 0
+   bestValidAccu = 0
+   trainLoss = 0
+   validLoss = 0
+   bestTrainLoss = math.huge
+   bestValidLoss = math.huge
+   bestTrainModel = nn.Sequential()
+   bestValidModel = nn.Sequential()
+   earlyStopCount = 0
+   for i=1, epochs do
+      if linearDecay then
+         optimState.learningRate = learningRates[i]
+      end
+      -- Training
+      trainLoss = model_train_multi_criterion(model, criterions,
+                                              parameters, gradParameters, trData,
+                                              optimMethod, optimState, batchSize,
+                                              i, confusion, trainLogger,
+                                              useCuda, displayProgress,
+                                              classiferIndx)
+      confusion:updateValids()
+      if loss then
+         if verbose then
+            print("Current train loss: ".. trainLoss
+                     ..", best train loss: " .. bestTrainLoss)
+         end
+         if trainLoss < bestTrainLoss then
+            bestTrainLoss = trainLoss
+            bestTrainModel = model:clone()
+            print(confusion)
+         end
+      else -- Using classification accuracy for saving best train model
+         trainAccu = confusion.totalValid * 100
+         if bestTrainAccu < trainAccu then
+            bestTrainAccu = trainAccu
+            bestTrainModel = model:clone()
+            bestTrainLoss = trainLoss
+         end
+         if verbose then
+            print("Current train accu: ".. trainAccu
+                     ..", best train accu: " .. bestTrainAccu
+                     ..", best train loss: " .. bestTrainLoss)
+         end
+      end
+
+      -- Validating
+      if not noValidation then
+         validLoss = model_test_multi_criterion(model, criterions,
+                                                tvData, confusion,
+                                                useCuda, classifierIndx)
+         confusion:updateValids()
+         if loss then
+            if verbose then
+               print("Current valid loss: ".. validLoss
+                        ..", best valid loss: " .. bestValidLoss)
+            end
+            if validLoss < bestValidLoss then
+               earlyStopCount = 0
+               bestValidLoss = validLoss
+               bestValidModel = model:clone()
+               print(confusion)
+            else
+               earlyStopCount = earlyStopCount + 1
+            end
+         else
+            validAccu = confusion.totalValid * 100
+            if bestValidAccu < validAccu then
+               earlyStopCount = 0
+               bestValidAccu = validAccu
+               bestValidModel = model:clone()
+               bestValidLoss = validLoss
+            else
+               earlyStopCount = earlyStopCount + 1
+            end
+            if verbose then
+               print("Current valid accu: ".. validAccu
+                     ..", best valid accu: " .. bestValidAccu
+                     ..", best valid loss: " .. bestValidLoss)
+            end
+         end
+         if verbose then
+            print(noiseSigma, weightTied, useBatchNorm, eta, earlyStopCount)
+         end
+      end
+
+      if maxTries ~= 0 then
+         if earlyStopCount >= maxTries then
+            if verbose then print("Early stopping at epoch: " .. i) end
+            break
+         end
       end
    end
+
+   -- Testing
+   testLoss = model_test_multi_criterion(bestValidModel, criterions,
+                                         tsData, confusion,
+                                         useCuda, classifierIndx)
+   confusion:updateValids()
+   testAccu = confusion.totalValid * 100
+   testAccus[attempt] = testAccu
 end
-
--- Testing
-testLoss = model_test_multi_criterion(bestValidModel, criterions,
-                                      tsData, confusion,
-                                      useCuda, classifierIndx)
-print("Testing Loss: " .. testLoss)
-confusion:updateValids()
-testAccu = confusion.totalValid * 100
-print("Testing confusion using best validation model.")
-print(confusion)
-print(noiseSigma, weightTied, useBatchNorm, eta)
+print("Test accuracies.")
+print(testAccus)
