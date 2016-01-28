5f8652125bfd22fff0d3123944d2d8a5a7bc358b
Sagar M Waghmare <SW@discoverelement.com>
2016-01-28 13:07:22 -0500

    Fixed encoder/classifier missing Add and CMul.

--- tutorials/ladder_network/ladder.lua
+++ tutorials/ladder_network/ladder.lua
@@ -85,7 +85,13 @@ op:summarize()
 -- Data
 noValidation = opt.noValidation
 best = opt.best
+verbose = opt.verbose
 
+   -- Cuda
+useCuda = opt.useCuda
+deviceId = tonumber(opt.deviceId)
+
+-- MNIST Data source
 ds = dp.Mnist{}
 
 attempts = tonumber(opt.attempts)
@@ -110,24 +116,23 @@ for attempt=1,attempts do
    tsData.size = function() return tsData.data:size()[1] end
    collectgarbage()
 
-   tempSample = trData.data[1]
-   channels = tempSample:size(1)
-   width = tempSample:size(2)
-   height = tempSample:size(3)
-   linFeats = channels * height * width
+   local tempSample = trData.data[1]
+   local channels = tempSample:size(1)
+   local width = tempSample:size(2)
+   local height = tempSample:size(3)
+   local linFeats = channels * height * width
 
    -- MNIST
-   classes = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
-   confusion = optim.ConfusionMatrix(classes)
+   local classes = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
+   local confusion = optim.ConfusionMatrix(classes)
 
    -- Model
-   noOfClasses = tonumber(opt.noOfClasses)
-   noiseSigma = tonumber(opt.noiseSigma)
-   inputHiddens = dp.returnString(opt.hiddens)
-   useBatchNorm = opt.useBatchNorm
-   weightTied = opt.weightTied
+   local noOfClasses = tonumber(opt.noOfClasses)
+   local noiseSigma = tonumber(opt.noiseSigma)
+   local inputHiddens = dp.returnString(opt.hiddens)
+   local useBatchNorm = opt.useBatchNorm
+   local weightTied = opt.weightTied
 
-   verbose = opt.verbose
 
    hiddens = {linFeats}
    for i=1,#inputHiddens do
@@ -136,6 +141,7 @@ for attempt=1,attempts do
    hiddens[#hiddens+1] = noOfClasses
 
    -- encoder input
+   local input = nil
    if noiseSigma ~= 0 then
       if verbose then print("Add noise to the samples.") end
       input = nn.WhiteNoise(0, noiseSigma)()
@@ -144,91 +150,102 @@ for attempt=1,attempts do
    end
 
    -- encoder model
-   encoderLayers = {}
-   Zs = {}
-   Zs[0] = input
-   for i=1,#hiddens-1 do
-      encoderLayers[i] = nn.Linear(hiddens[i], hiddens[i+1])
-      if i==1 then
-         if useBatchNorm then
-            Zs[i] = nn.BatchNormalization(hiddens[i+1])(encoderLayers[i](input))
-         else
-            Zs[i] = encoderLayers[i](input)
-         end
+   local encoderLayers = {}
+   local Zs = {}
+   Zs[1] = input
+   local Hs = {}
+   Hs[1] = input
+   for i=2,#hiddens do
+      -- Zs
+      encoderLayers[i] = nn.Linear(hiddens[i-1], hiddens[i])
+      if useBatchNorm then
+         Zs[i] = nn.BatchNormalization(hiddens[i])
+                                      (encoderLayers[i](Hs[i-1]))
       else
-         if useBatchNorm then
-            Zs[i] = nn.BatchNormalization(hiddens[i+1])
-                                         (encoderLayers[i](nn.ReLU()(Zs[i-1])))
-         else
-            Zs[i] = encoderLayers[i](nn.ReLU()(Zs[i-1]))
-         end
+         Zs[i] = encoderLayers[i](Hs[i-1])
+      end
+     
+      -- Hs
+      if i==#hiddens then
+         Hs[i] = nn.CMul(hiddens[i])(nn.Add(hiddens[i])(Zs[i]))
+      else
+         Hs[i] = nn.ReLU()(nn.CMul(hiddens[i])(nn.Add(hiddens[i])(Zs[i])))
       end
    end
 
    -- classifier
-   classifier = nn.LogSoftMax()(Zs[#Zs])
+   local classifier = nn.LogSoftMax()(Hs[#Hs])
 
    -- Decoder
-   decoderLayers = {}
-   Z_hats = {}
-   Z_hats[#hiddens+1] = Zs[#Zs]
-   for i=#hiddens,2,-1 do
-      decoderLayers[i] = nn.Linear(hiddens[i], hiddens[i-1])
-      if weightTied then
-         if verbose then print("Tying encoder-decoder weights.") end
-         decoderLayers[i].weight:set(encoderLayers[i-1].weight:t())
-         decoderLayers[i].gradWeight:set(encoderLayers[i-1].gradWeight:t())
-      end
-      if useBatchNorm then
-         u = nn.ReLU()(nn.BatchNormalization(hiddens[i-1])
-                                            (decoderLayers[i](Z_hats[i+1])))
+   local decoderLayers = {}
+   local Z_hats = {}
+   for i=#hiddens,1,-1 do
+
+      -- u = 0 hence no cij
+      if i==#hiddens then
+         z_hat1 = nn.CMul(hiddens[i])(Zs[i])
+         z_hat2 = nn.CMul(hiddens[i])(Zs[i])
+         z_hat3 = nn.CMul(hiddens[i])(Zs[i])
+         z_hat34 = nn.Add(hiddens[i])(z_hat3)
+         z_hatSigmoid34 = nn.Sigmoid()(z_hat34)
+         z_hat234 = nn.CMulTable()({z_hat2, z_hatSigmoid34})
+         z_hat5 = nn.CMul(hiddens[i])(Zs[i])
+         Z_hats[i] = nn.CAddTable()({z_hat1, z_hat234, z_hat5})
       else
-         u = nn.ReLU()(decoderLayers[i](Z_hats[i+1]))
+         decoderLayers[i] = nn.Linear(hiddens[i+1], hiddens[i])
+         if weightTied then
+            if verbose then print("Tying encoder-decoder weights.") end
+            decoderLayers[i].weight:set(encoderLayers[i+1].weight:t())
+            decoderLayers[i].gradWeight:set(encoderLayers[i+1].gradWeight:t())
+         end
+
+         u = decoderLayers[i](Z_hats[i+1])
+
+         cu1 = nn.CMul(hiddens[i])(u)
+         du1 = nn.Add(hiddens[i])(u)
+         a1 = nn.CAddTable()({cu1, du1})
+         cu2 = nn.CMul(hiddens[i])(u)
+         du2 = nn.Add(hiddens[i])(u)
+         a2 = nn.CAddTable()({cu2, du2})
+         cu3 = nn.CMul(hiddens[i])(u)
+         du3 = nn.Add(hiddens[i])(u)
+         a3 = nn.CAddTable()({cu3, du3})
+         cu4 = nn.CMul(hiddens[i])(u)
+         du4 = nn.Add(hiddens[i])(u)
+         a4 = nn.CAddTable()({cu4, du4})
+         cu5 = nn.CMul(hiddens[i])(u)
+         du5 = nn.Add(hiddens[i])(u)
+         a5 = nn.CAddTable()({cu5, du5})
+
+         z_hat1 = nn.CMulTable()({a1, Zs[i]})
+         z_hat2 = nn.CMulTable()({a3, Zs[i]})
+         z_hat3 = nn.Sigmoid()(nn.CAddTable()({z_hat2, a4}))
+         z_hat4 = nn.CMulTable()({a2, z_hat3})
+         Z_hats[i] = nn.CAddTable()({z_hat1, z_hat4, a5})
       end
-
-      cu1 = nn.CMul(hiddens[i-1])(u)
-      du1 = nn.Add(hiddens[i-1])(u)
-      a1 = nn.CAddTable()({cu1, du1})
-      cu2 = nn.CMul(hiddens[i-1])(u)
-      du2 = nn.Add(hiddens[i-1])(u)
-      a2 = nn.CAddTable()({cu2, du2})
-      cu3 = nn.CMul(hiddens[i-1])(u)
-      du3 = nn.Add(hiddens[i-1])(u)
-      a3 = nn.CAddTable()({cu3, du3})
-      cu4 = nn.CMul(hiddens[i-1])(u)
-      du4 = nn.Add(hiddens[i-1])(u)
-      a4 = nn.CAddTable()({cu4, du4})
-      cu5 = nn.CMul(hiddens[i-1])(u)
-      du5 = nn.Add(hiddens[i-1])(u)
-      a5 = nn.CAddTable()({cu5, du5})
-
-      z_hat1 = nn.CMulTable()({a1, Zs[i-2]})
-      z_hat2 = nn.CMulTable()({a3, Zs[i-2]})
-      z_hat3 = nn.Sigmoid()(nn.CAddTable()({z_hat2, a4}))
-      z_hat4 = nn.CMulTable()({a2, z_hat3})
-      Z_hats[i] = nn.CAddTable()({z_hat1, z_hat4, a5})
    end
-   model = nn.gModule({input}, {classifier, Z_hats[2]--[[Decoder--]]})
-   print(model)
+   local model = nn.gModule({input}, {classifier, Z_hats[1]--[[Decoder--]]})
+   if verbose then print(model) end
 
    -- Criterion and learning
    -- Criterion
-   eta = tonumber(opt.eta)
-   criterions = nn.ParallelCriterion()
-   nll = nn.ClassNLLCriterion()
-   mse = nn.MSECriterion()
+   local eta = tonumber(opt.eta)
+   local criterions = nn.ParallelCriterion()
+   local nll = nn.ClassNLLCriterion()
+   local mse = nn.MSECriterion()
    criterions:add(nll)
    criterions:add(mse, eta)
 
    -- Learning
-   batchSize = tonumber(opt.batchSize)
-   epochs = tonumber(opt.epochs)
-   maxTries = tonumber(opt.maxTries)
-   learningRate = tonumber(opt.learningRate)
-   learningRateDecay = tonumber(opt.learningRateDecay)
-   linearDecay = opt.linearDecay
-   startEpoch = tonumber(opt.startEpoch)
-   endLearningRate = tonumber(opt.endLearningRate)
+   local batchSize = tonumber(opt.batchSize)
+   local epochs = tonumber(opt.epochs)
+   local maxTries = tonumber(opt.maxTries)
+   local learningRate = tonumber(opt.learningRate)
+   local learningRateDecay = tonumber(opt.learningRateDecay)
+   local linearDecay = opt.linearDecay
+   local startEpoch = tonumber(opt.startEpoch)
+   local endLearningRate = tonumber(opt.endLearningRate)
+   assert(epochs > startEpoch, "startEpoch should be smaller than epochs.")   
 
    if linearDecay then
       if verbose then print("Using linear decay.") end
@@ -238,19 +255,19 @@ for attempt=1,attempts do
       learningRates = torch.cat(learningRates, temp)
    end
 
-   momentum = tonumber(opt.momentum)
-   loss = opt.loss
-   adam = opt.adam
+   local momentum = tonumber(opt.momentum)
+   local loss = opt.loss
+   local adam = opt.adam
 
    -- Optimizer
-   optimState = {
-                  coefL1 = 0,
-                  coefL2 = 0,
-                  learningRate = learningRate,
-                  weightDecay = 0.0,
-                  momentum = momentum,
-                  learningRateDecay = learningRateDecay
-                }
+   local optimState = {
+                       coefL1 = 0,
+                       coefL2 = 0,
+                       learningRate = learningRate,
+                       weightDecay = 0.0,
+                       momentum = momentum,
+                       learningRateDecay = learningRateDecay
+                      }
 
    -- If true use Adaptive moment estimation else SGD.
    if adam then
@@ -265,9 +282,6 @@ for attempt=1,attempts do
       print(optimState)
    end
 
-   -- Cuda
-   useCuda = opt.useCuda
-   deviceId = tonumber(opt.deviceId)
 
    if useCuda then
       if verbose then print("Using GPU: "..deviceId) end
@@ -305,19 +319,19 @@ for attempt=1,attempts do
    end
 
    -- Training
-   displayProgress = verbose
-   classifierIndx = 1
-   trainAccu = 0
-   validAccu = 0
-   bestTrainAccu = 0
-   bestValidAccu = 0
-   trainLoss = 0
-   validLoss = 0
-   bestTrainLoss = math.huge
-   bestValidLoss = math.huge
-   bestTrainModel = nn.Sequential()
-   bestValidModel = nn.Sequential()
-   earlyStopCount = 0
+   local displayProgress = verbose
+   local classifierIndx = 1
+   local trainAccu = 0
+   local validAccu = 0
+   local bestTrainAccu = 0
+   local bestValidAccu = 0
+   local trainLoss = 0
+   local validLoss = 0
+   local bestTrainLoss = math.huge
+   local bestValidLoss = math.huge
+   local bestTrainModel = nn.Sequential()
+   local bestValidModel = nn.Sequential()
+   local earlyStopCount = 0
    for i=1, epochs do
       if linearDecay then
          optimState.learningRate = learningRates[i]
@@ -421,6 +435,9 @@ for attempt=1,attempts do
    confusion:updateValids()
    testAccu = confusion.totalValid * 100
    testAccus[attempt] = testAccu
+   if verbose then
+      print("Attempt: " .. tostring(attempt) .. " Test Accu: " .. testAccu)
+   end
 end
 print("Test accuracies.")
 print(testAccus)
